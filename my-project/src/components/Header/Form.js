1832e56976bd5a85699fe6af58812b28a189184a
Landon Ross <landon.w.ross@gmail.com>
2021-06-07 20:51:17 -0600

    added tailwinds.css features and page layout

---
+++ my-project/src/components/Header/Form.js
@@ -0,0 +1,138 @@
+export default function Form() {
+    return (
+      <div className="space-y-6">  
+        <div className="bg-white shadow px-4 py-5 sm:rounded-lg sm:p-6">
+          <div className="md:grid md:grid-cols-3 md:gap-6">
+            <div className="md:col-span-1">
+              <h3 className="text-lg font-medium leading-6 text-gray-900">Home Search Parameters</h3>
+              <p className="mt-1 text-sm text-gray-500">Search for homes by city and price</p>
+            </div>
+            <div className="mt-5 md:mt-0 md:col-span-2">
+              <form action="#" method="POST">
+                <div className="grid grid-cols-6 gap-6">
+                  {/* <div className="col-span-6 sm:col-span-3">
+                    <label htmlFor="first_name" className="block text-sm font-medium text-gray-700">
+                      City
+                    </label>
+                    <input
+                      type="text"
+                      name="first_name"
+                      id="first_name"
+                      autoComplete="given-name"
+                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    />
+                  </div>
+  
+                  <div className="col-span-6 sm:col-span-3">
+                    <label htmlFor="last_name" className="block text-sm font-medium text-gray-700">
+                      Last name
+                    </label>
+                    <input
+                      type="text"
+                      name="last_name"
+                      id="last_name"
+                      autoComplete="family-name"
+                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    />
+                  </div>
+  
+                  <div className="col-span-6 sm:col-span-4">
+                    <label htmlFor="email_address" className="block text-sm font-medium text-gray-700">
+                      Email address
+                    </label>
+                    <input
+                      type="text"
+                      name="email_address"
+                      id="email_address"
+                      autoComplete="email"
+                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    />
+                  </div>
+  
+                  <div className="col-span-6 sm:col-span-3">
+                    <label htmlFor="country" className="block text-sm font-medium text-gray-700">
+                      Country / Region
+                    </label>
+                    <select
+                      id="country"
+                      name="country"
+                      autoComplete="country"
+                      className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    >
+                      <option>United States</option>
+                      <option>Canada</option>
+                      <option>Mexico</option>
+                    </select>
+                  </div>
+  
+                  <div className="col-span-6">
+                    <label htmlFor="street_address" className="block text-sm font-medium text-gray-700">
+                      Street address
+                    </label>
+                    <input
+                      type="text"
+                      name="street_address"
+                      id="street_address"
+                      autoComplete="street-address"
+                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    />
+                  </div> */}
+  
+                  <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                    <label htmlFor="city" className="block text-sm font-medium text-gray-700">
+                      City
+                    </label>
+                    <input
+                      type="text"
+                      name="city"
+                      id="city"
+                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    />
+                  </div>
+  
+                  <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                    <label htmlFor="state" className="block text-sm font-medium text-gray-700">
+                      State / Province
+                    </label>
+                    <input
+                      type="text"
+                      name="state"
+                      id="state"
+                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    />
+                  </div>
+  
+                  <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                    <label htmlFor="postal_code" className="block text-sm font-medium text-gray-700">
+                      Price Range
+                    </label>
+                    <input
+                      type="text"
+                      name="postal_code"
+                      id="postal_code"
+                      autoComplete="postal-code"
+                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    />
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-end">
+          <button
+            type="button"
+            className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Save
+          </button>
+        </div>
+      </div>
+    )
+  }
---
+++ my-project/src/components/Header/Header.js
@@ -0,0 +1,204 @@
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import './index.css';
+import Cards from "../Cards/Cards";
+import Form from "./Form.js";
+
+const navigation = ['Home', 'Zillow', 'About', 'Contact']
+const profile = ['Your Profile', 'Settings', 'Sign out']
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Header() {
+  return (
+    <div>
+      <div className="bg-gray-800 pb-32">
+        <Disclosure as="nav" className="bg-gray-800">
+          {({ open }) => (
+            <>
+              <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div className="border-b border-gray-700">
+                  <div className="flex items-center justify-between h-16 px-4 sm:px-0">
+                    <div className="flex items-center">
+                      <div className="flex-shrink-0">
+                        <img
+                          className="h-10 w-10"
+                          src="./btr.png"
+                          alt="BTR"
+                        />
+                      </div>
+                      <div className="hidden md:block">
+                        <div className="ml-10 flex items-baseline space-x-4">
+                          {navigation.map((item, itemIdx) =>
+                            itemIdx === 0 ? (
+                              <Fragment key={item}>
+                                {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+                                <a href="/" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
+                                  {item}
+                                </a>
+                              </Fragment>
+                            ) : (
+                              <a
+                                key={item}
+                                href="/"
+                                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                              >
+                                {item}
+                              </a>
+                            )
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                    <div className="hidden md:block">
+                      <div className="ml-4 flex items-center md:ml-6">
+                        <button className="bg-gray-800 p-1 text-gray-400 rounded-full hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                          <span className="sr-only">View notifications</span>
+                          <BellIcon className="h-6 w-6" aria-hidden="true" />
+                        </button>
+
+                        {/* Profile dropdown */}
+                        <Menu as="div" className="ml-3 relative">
+                          {({ open }) => (
+                            <>
+                              <div>
+                                <Menu.Button className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                                  <span className="sr-only">Open user menu</span>
+                                  <img
+                                    className="h-8 w-8 rounded-full"
+                                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                    alt=""
+                                  />
+                                </Menu.Button>
+                              </div>
+                              <Transition
+                                show={open}
+                                as={Fragment}
+                                enter="transition ease-out duration-100"
+                                enterFrom="transform opacity-0 scale-95"
+                                enterTo="transform opacity-100 scale-100"
+                                leave="transition ease-in duration-75"
+                                leaveFrom="transform opacity-100 scale-100"
+                                leaveTo="transform opacity-0 scale-95"
+                              >
+                                <Menu.Items
+                                  static
+                                  className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                                >
+                                  {profile.map((item) => (
+                                    <Menu.Item key={item}>
+                                      {({ active }) => (
+                                        <a
+                                          href="/"
+                                          className={classNames(
+                                            active ? 'bg-gray-100' : '',
+                                            'block px-4 py-2 text-sm text-gray-700'
+                                          )}
+                                        >
+                                          {item}
+                                        </a>
+                                      )}
+                                    </Menu.Item>
+                                  ))}
+                                </Menu.Items>
+                              </Transition>
+                            </>
+                          )}
+                        </Menu>
+                      </div>
+                    </div>
+                    <div className="-mr-2 flex md:hidden">
+                      {/* Mobile menu button */}
+                      <Disclosure.Button className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                        <span className="sr-only">Open main menu</span>
+                        {open ? (
+                          <XIcon className="block h-6 w-6" aria-hidden="true" />
+                        ) : (
+                          <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                        )}
+                      </Disclosure.Button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <Disclosure.Panel className="border-b border-gray-700 md:hidden">
+                <div className="px-2 py-3 space-y-1 sm:px-3">
+                  {navigation.map((item, itemIdx) =>
+                    itemIdx === 0 ? (
+                      <Fragment key={item}>
+                        {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+                        <a href="/" className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">
+                          {item}
+                        </a>
+                      </Fragment>
+                    ) : (
+                      <a
+                        key={item}
+                        href="/"
+                        className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                      >
+                        {item}
+                      </a>
+                    )
+                  )}
+                </div>
+                <div className="pt-4 pb-3 border-t border-gray-700">
+                  <div className="flex items-center px-5">
+                    <div className="flex-shrink-0">
+                      <img
+                        className="h-10 w-10 rounded-full"
+                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        alt=""
+                      />
+                    </div>
+                    <div className="ml-3">
+                      <div className="text-base font-medium leading-none text-white">Tom Cook</div>
+                      <div className="text-sm font-medium leading-none text-gray-400">tom@example.com</div>
+                    </div>
+                    <button className="ml-auto bg-gray-800 flex-shrink-0 p-1 text-gray-400 rounded-full hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                      <span className="sr-only">View notifications</span>
+                      <BellIcon className="h-6 w-6" aria-hidden="true" />
+                    </button>
+                  </div>
+                  <div className="mt-3 px-2 space-y-1">
+                    {profile.map((item) => (
+                      <a
+                        key={item}
+                        href="/"
+                        className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
+                      >
+                        {item}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+        <header className="py-10">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold text-white">Dashboard</h1>
+          </div>
+        </header>
+      </div>
+
+      <main className="-mt-32">
+        <div className="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8">
+          {/* Replace with your content */}
+          <div className="bg-white rounded-lg shadow px-5 py-6 sm:px-6">
+            <Form />
+            <Cards />
+  {/* <div id="zillow-large-search-box-widget-container"><h2 id="zillow-2">Find Homes</h2><div style={{float:"right"}}><a href="https://www.zillow.com/" target="_blank" rel="noreferrer"><img alt="Zillow Real Estate Information" style={{border:0}} src="https://www.zillow.com/widgets/GetVersionedResource.htm?path=/static/images/powered-by-zillow.gif"></img></a></div><iframe scrolling="no" src="https://www.zillow.com/widgets/search/LargeSearchBoxWidget.htm?did=zillow-large-search-box-iframe-widget&type=iframe&rgname=Seattle+WA&shvi=yes" width="430" frameborder="0" height="400"></iframe><table id="zillow-tnc-widget-footer-links" width="100%"><tbody style={{margin:0 ,padding:0}}><tr style={{margin:0, padding:0}}><td style={{fontWeight:"bold", fontSize:10, color:"#555", textAlign:"left", margin:0, padding:0}}>QUICK LINKS:</td></tr><tr style={{margin:0, padding:0}}><td style={{margin:0, padding:0}}><span id="widgetFooterLink" className="regionBasedLink"><a href="https://www.zillow.com/salt-wa/" target="_blank" rel="noReferrer" style={{color:"#36b", fontFamily:"verdana,arial,sans-serif", fontSize:"10px", margin:"0 5px 0 0", padding:0, textDecoration:"none"}}><span className="region">Seattle</span> Real Estate Listing</a></span></td><td style={{margin:0, padding:0}}><span id="widgetFooterLink"><a href="https://www.zillow.com/mortgage-rates/" target="_blank" rel="noReferrer" style={{color:"#36b", fontFamily:"verdana,arial,sans-serif", fontSize:"10px", margin:"0 5px 0 0", padding:0, textDecoration:"none"}}>Mortgage Rates</a></span></td><td style={{margin:0, padding:0}}><span id="widgetFooterLink"><a href="https://www.zillow.com/refinance/" target="_blank" rel="noReferrer" style={{color:"#36b", fontFamily:"verdana,arial,sans-serif", fontSize:"10px", margin:"0 5px 0 0", padding:0, textDecoration:"none"}}>Refinancing</a></span></td></tr><tr style={{margin:0, padding:0}}><td style={{margin:0, padding:0}}><span id="widgetFooterLink" className="regionBasedLink"><a href="https://www.zillow.com/seattle-wa/foreclosures/" target="_blank" rel="noReferrer" style={{color:"#36b", fontSize:"10px", margin:"0 5px 0 0", padding:0, textDecoration:"none"}}><span className="region">Seattle</span> Foreclosures</a></span></td><td style={{margin:0, padding:0}}><span id="widgetFooterLink"><a href="https://www.zillow.com/mortgage-calculator/" target="_blank" rel="noReferrer" style={{color:"#36b", fontSize:"10px", margin:"0 5px 0 0", padding:0, textDecoration:"none"}}>Mortgage Calculators</a></span></td><td style={{margin:0, padding:0}}><span id="widgetFooterLink"><a href="https://www.zillow.com/mortgage-rates/" target="_blank" rel="noReferrer" style={{color:"#36b", fontSize:"10px", margin:"0 5px 0 0", padding:0, textDecoration:"none"}}>Purchase Loans</a></span></td></tr></tbody></table></div> */}
+            {/* <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" /> */}
+          </div>
+          {/* /End replace */}
+        </div>
+      </main>
+    </div>
+  )
+}
