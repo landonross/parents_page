b27dcff6007c4f62630bbf26f6715360972cfb2e
Landon Ross <landon.w.ross@gmail.com>
2021-06-15 13:03:32 -0600

    trying to run server

--- server.js
+++ server.js
@@ -1,32 +1,22 @@
 const express = require("express");
 
 // const mongoose = require("mongoose");
-// const routes = require("./routes/apiRoutes");
+const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const path = require('path');
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// } else {
-//   app.use(express.static("client"));
-// }
-// Add routes, both API and view
-// app.use('/api', routes);
-
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+// Add routes, both API and view
+app.use(routes);
 
 // Connect to the Mongo DB
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/garden-overflow");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactcms");
 
 // Start the API server
 app.listen(PORT, function() {
